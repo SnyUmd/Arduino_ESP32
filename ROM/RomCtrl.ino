7d5e82157448ac8a17d3d50f0dc39deca61a5906
Shinya Umeda <shumeda@iot-sq.com>
2020-07-06 12:53:49 +0900

    no message

---
+++ ROM/RomCtrl.ino
@@ -0,0 +1,22 @@
+#define LED_BLUE 2
+#define BOARD_SW 0
+
+#define LED_ON HIGH
+#define LED_OFF LOW
+
+#define SW_ON LOW
+#define SW_OFF HIGH
+
+#include <Arduino.h>
+String wrfile = "/led_sts.txt";//読み書きするファイル名を設定
+
+//*******************************************************
+void setup()
+{
+    Serial.begin(115200);
+}
+
+//*******************************************************
+void loop() 
+{
+}
