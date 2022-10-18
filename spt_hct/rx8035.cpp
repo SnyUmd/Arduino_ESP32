efb0642777c2c1ee7f93dd96a0c4d3c061ce4ea7
Shinya.Umeda <umeda@barnet.co.jp>
2022-10-19 08:31:31 +0900

    no message

--- spt_hct/rx8035.cpp
+++ spt_hct/rx8035.cpp
@@ -1,1 +1,11 @@
 #include "rx8035.h"
+
+void readI2C(byte* val, TwoWire& wire_, int addr, int readAddr, int read_to_byte)
+{
+    wire_.beginTransmission(addr);
+    wire_.write(readAddr);
+    wire_.endTransmission(false);
+
+    wire_.requestFrom(addr, read_to_byte, true);
+    for(int i = 0; i < read_to_byte; i++) val[i] = wire_.read();
+}
--- spt_hct/spt_hct.ino
+++ spt_hct/spt_hct.ino
@@ -4,6 +4,7 @@
 #include "Timer.h"
 #include "wifiCtrl.h"
 #include "ledCtrl.h"
+#include "rx8035.h"
 
 // hw_timer_t * timer = NULL;
 struct tm timeInf;
@@ -81,15 +82,7 @@ void funcInterrupt()
    
 }
 
-void readI2C(byte* val ,TwoWire& wire_, int addr, int readAddr, int read_to_byte)
-{
-    wire_.beginTransmission(addr);
-    wire_.write(readAddr);
-    wire_.endTransmission(false);
 
-    wire_.requestFrom(addr, read_to_byte, true);
-    for(int i = 0; i < read_to_byte; i++) val[i] = wire_.read();
-}
 
 //***************************************************************************************************************
 //***************************************************************************************************************
